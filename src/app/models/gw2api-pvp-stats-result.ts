29801118a17d8f32c5caebde3afbe243f03341d9
Stéphane Lasmartres <SLasmartres@idt.pf>
2020-07-24 13:35:10 -1000

    endpoint results for pvp stats has been completed

--- src/app/models/gw2api-pvp-stats-result.ts
+++ src/app/models/gw2api-pvp-stats-result.ts
@@ -4,6 +4,23 @@ export interface Gw2ApiPvpStatsResults {
     pvp_rank_points: number;
     pvp_rank_rollovers: number;
     aggregate: WinLossStats;
+    professions: {
+        elementalist: WinLossStats,
+        engineer: WinLossStats,
+        guardian: WinLossStats,
+        mesmer: WinLossStats,
+        necromancer: WinLossStats,
+        ranger: WinLossStats,
+        thief: WinLossStats,
+        warrior: WinLossStats,
+    },
+    ladders: {
+        none: WinLossStats,
+        ranked: WinLossStats,
+        soloarenarated: WinLossStats,
+        teamarenarated: WinLossStats,
+        unranked: WinLossStats
+    }
 }
 
 export interface WinLossStats {
